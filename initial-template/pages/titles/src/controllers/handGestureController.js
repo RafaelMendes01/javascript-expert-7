0de3edebe7790a06f8e7d599837300692e6e1796
rafael mendes <rafael.mendes@tallos.com.br>
2023-03-04 17:44:52 -0300

    feat: class 2 checkpoint

---
+++ initial-template/pages/titles/src/controllers/handGestureController.js
@@ -0,0 +1,67 @@
+//comigo não funcionou essa abordagem
+// import { prepareRunChecker } from "../../../../lib/shared/util"
+// const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+
+export default class HandGestureController {
+    #view
+    #service
+    #camera
+    #continues = false
+    #lastDirection = {
+        direction: '',
+        y: 0
+    }
+    constructor({ view, service, camera }) {
+        this.#view = view
+        this.#service = service
+        this.#camera = camera
+    }
+    async init() {
+        return this.#loop()
+    }
+    #scrollPage(direction) {
+        const pixelsPerScroll = 100
+        if (this.#lastDirection.direction === direction) {
+            this.#lastDirection.y = (
+                direction === 'scroll-down' ?
+                    this.#lastDirection.y + pixelsPerScroll
+                    :
+                    this.#lastDirection.y - pixelsPerScroll
+            )
+        }
+        else {
+            this.#lastDirection.direction = direction
+        }
+
+        this.#view.scrollPage(this.#lastDirection.y)
+
+    }
+    async #estimateHands() {
+        try {
+            const hands = await this.#service.estimateHands(this.#camera.video)
+            for await (const { event, x, y } of this.#service.detectGestures(hands)) {
+                if (event.includes('scroll')) {
+                    setTimeout(() => {
+                       this.#continues = true
+                    }, 200)
+                    if (!this.#continues) continue;
+                    this.#scrollPage(event)
+                    this.#continues = false
+                }
+            }
+        } catch (error) {
+            console.error('deu ruim', error)
+
+        }
+    }
+
+    async #loop() {
+        await this.#service.initializeDetector()
+        await this.#estimateHands()
+        this.#view.loop(this.#loop.bind(this))
+    }
+    static async initialize(deps) {
+        const controller = new HandGestureController(deps)
+        return controller.init()
+    }
+}
